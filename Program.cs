e5d3d998ae3149598b046e9bc90d011c1290c1fb
pedro Okuma <pedrookuma3@gmail.com>
2020-12-01 19:30:37 -0300

    Melhoria estetica(Ação) feita

--- Program.cs
+++ Program.cs
@@ -50,6 +50,7 @@ namespace Personagens
             Console.WriteLine("[3] - Defesa (Diminui o dano recebido em 20)");
             Console.WriteLine("[4] - Cura (Cura de 10 a 40 pontos de vida)");
             do{
+            Console.write("Ação: ");
             jogador1.Escolha = int.Parse(Console.ReadLine());
 
             if (jogador1.vida >= 100 && jogador1.Escolha == 4){
